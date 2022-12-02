10adf180851396565061bcc40065c63ea973f8d0
Adauto Bueno <adauto.bueno@gmail.com>
2022-12-01 23:40:51 -0300

    Atualizado classe principal

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -2,9 +2,10 @@ package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DsmetaApplication {
+public class DsmetaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsmetaApplication.class, args);
